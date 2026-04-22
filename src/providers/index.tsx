4525bf8569bccb01d4f85c89edf6cde9f7746da0
allankevinscain <meuemail44allan@gmail.com>
2026-04-22 13:40:18 -0300

    fix: sem refetch

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,8 +1,16 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ThemeProvider } from "./theme";
 import { AuthProvider } from "./auth/provider";
+import { ThemeProvider } from "./theme";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
 
 interface ProvidersProps {
   children: React.ReactNode;
