45c2fb597ebf3328e5c7000be1efe0b29ed9d69d
allankevinscain <meuemail44allan@gmail.com>
2026-09-03 07:01:27 -0300

    fix: lint

--- src/components/header/flip-icon.tsx
+++ src/components/header/flip-icon.tsx
@@ -1,14 +1,9 @@
-
 export function FlipIcon() {
-
   return (
-        <a href="/about-me">
-          <div
-          className="w-10 h-10bg-(--color-border)"
-          style={{ backfaceVisibility: "hidden" }}
-        >
-          <img src="/reacao-4.webp" alt="code" />
-        </div>
-        </a>
+    <a href="/about-me">
+      <div className="h-10bg-(--color-border) w-10" style={{ backfaceVisibility: "hidden" }}>
+        <img src="/reacao-4.webp" alt="code" />
+      </div>
+    </a>
   );
 }
